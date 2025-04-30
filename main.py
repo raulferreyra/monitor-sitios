9deea82413905a32709553a91bb6a563503f0d73
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-04-30 12:29:50 -0500

    Add Icon, MessageBox and Placeholders

--- main.py
+++ main.py
@@ -3,13 +3,42 @@ from tkinter import messagebox
 
 
 class App:
+    def open_config(self):
+        # This method will replace the configuration modal
+        messagebox.showinfo(
+            "Configuración", "Aquí irá la ventana de configuración.")
+
+    def hide_window(self):
+        # Hide Main Window
+        self.root.withdraw()
+
+    def create_widgets(self):
+        # Welcome Label
+        title = tk.Label(
+            self.root, text="US - Monitor de Sitios", font=("Arial", 24))
+        title.pack(pady=30)
+
+        # Button for open conf
+        config_button = tk.Button(self.root, text="Configuración", font=(
+            "Arial", 14), command=self.open_config)
+        config_button.pack(pady=10)
+
+        # Placeholder for table
+        placeholder = tk.Label(
+            self.root, text="Aquí se mostrarán los sitios monitoreados.", font=("Arial", 12))
+        placeholder.pack(pady=20)
+
     def __init__(self, root):
         self.root = root
         self.root.title("US - Monitor de Sitios")
         self.root.geometry("800x600")
+        self.root.protocol("WM_DELETE_WINDOW", self.hide_window)
+
+        self.create_widgets()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.iconbitmap("favicon.ico")
     app = App(root)
     root.mainloop()
